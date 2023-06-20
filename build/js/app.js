164756e7a732c5878fb1eeb767f4f4795144dc7d
João <joaovitorleonardo8@gmail.com>
2023-06-20 11:09:38 -0300

    primeiro commit

---
+++ build/js/app.js
@@ -0,0 +1,9 @@
+/*const sr = ScrollReveal({reset: true})
+
+sr.reveal("#secondary-section", {
+    origin: "left", //Para ele surgir de cima para baixo.
+    distance: "150px", // A distancia do origin para a posição original do elemento.
+    duration: 5000, // A duração da animação em ms
+    delay: 500, // O delay para a animação acontecer
+    reset: true // Ao sair do campo de tela, a animação não acontecerá novamente caso eu passe por ela novamente.
+}); */
