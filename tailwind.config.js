50bf896cb43773752c4b74fe6b1528ef7c5805f0
João <joaovitorleonardo8@gmail.com>
2023-06-28 15:40:46 -0300

    Responsive Phone

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 
 module.exports = {
-  content: ["./src/build/*.{html,js}",
-            "./src/build/js/*.{html,js}", './index.html',
-],
+  content: ["./src/build/*.{html,js}", "./src/build/js/*.{html,js}", './index.html',],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
     extend: {},
   },
   plugins: [],
