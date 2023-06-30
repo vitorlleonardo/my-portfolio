5f0c5ac2822fdd17cf16465652ea039a061123ce
João <joaovitorleonardo8@gmail.com>
2023-06-30 14:07:14 -0300

    Responsividade ate 480px

--- src/build/js/app.js
+++ src/build/js/app.js
@@ -1,6 +1,16 @@
-/*const sr = ScrollReveal({reset: true})
+/* const sr = ScrollReveal({reset: true})
 
-sr.reveal("#secondary-section", {
+ sr.reveal("#secondary-section", {
+    origin: "left", //Para ele surgir de cima para baixo.
+    distance: "250px", // A distancia do origin para a posição original do elemento.
+    duration: 4000, // A duração da animação em ms
+    delay: 300, // O delay para a animação acontecer
+    reset: true // Ao sair do campo de tela, a animação não acontecerá novamente caso eu passe por ela novamente.
+}); 
+
+const mg = ScrollReveal({reset: true})
+
+mg.reveal("#third-section", {
     origin: "left", //Para ele surgir de cima para baixo.
     distance: "150px", // A distancia do origin para a posição original do elemento.
     duration: 5000, // A duração da animação em ms
