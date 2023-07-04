ad494e52b155d447f4ad566033abb13a66b4fa4f
João <joaovitorleonardo8@gmail.com>
2023-07-04 10:03:47 -0300

    v.1.0

--- src/build/js/app.js
+++ src/build/js/app.js
@@ -1,19 +1,5 @@
-/* const sr = ScrollReveal({reset: true})
+let language = document.getElementById("language");
 
- sr.reveal("#secondary-section", {
-    origin: "left", //Para ele surgir de cima para baixo.
-    distance: "250px", // A distancia do origin para a posição original do elemento.
-    duration: 4000, // A duração da animação em ms
-    delay: 300, // O delay para a animação acontecer
-    reset: true // Ao sair do campo de tela, a animação não acontecerá novamente caso eu passe por ela novamente.
-}); 
-
-const mg = ScrollReveal({reset: true})
-
-mg.reveal("#third-section", {
-    origin: "left", //Para ele surgir de cima para baixo.
-    distance: "150px", // A distancia do origin para a posição original do elemento.
-    duration: 5000, // A duração da animação em ms
-    delay: 500, // O delay para a animação acontecer
-    reset: true // Ao sair do campo de tela, a animação não acontecerá novamente caso eu passe por ela novamente.
-}); */
+language.addEventListener("click", () => {
+    alert("Em Desenvolvimento");
+});
